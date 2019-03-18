1f7780ed9467a3f7e4c7e4508f727528743708d6
Dhananjay Kajla <dhananjaykajla@protonmail.com>
2019-03-18 18:18:34 +0530

    almost all code functioning

--- wallet.cpp
+++ wallet.cpp
@@ -2,6 +2,26 @@
 #include <ctime>
 #include "wallet.h"
 #include <stdlib.h>
+bool is_sub_string(std::string a,std::string b)
+{
+    for(int i=0;i<b.size()-a.size();i++)
+    {
+        int j=0;
+        for(;j<a.size();j++)
+        {
+            if(a[j]==b[i+j])
+            {
+                continue;
+            }
+            break;
+        }
+        if(j==a.size())
+        {
+            return true;
+        }
+        return false;
+    }
+}
 std::vector<std::string> tokenizer(std::string str)
 {
     std::vector<std::string> vs;
@@ -48,10 +68,11 @@ wallet::wallet(std::string file_name)
     while(getline(infile,line))
     {
         std::vector<std::string> tokens = tokenizer(line);
-        std::string str,str1;
+        std::string str,str1,str2;
         getline(infile,str);
         getline(infile,str1);
-        history[i] = make_tuple(stoi(tokens[0]),tokens[1],stod(tokens[2]),str,str1,stod(tokens[3]));
+        getline(infile,str2);
+        history[i] = {tokens[0],str,tokens[1],str1,str2,tokens[2]};
         i++;
     }
     std::ofstream outfile,outfile1;
@@ -81,6 +102,7 @@ wallet::wallet(std::string file_name,std::string wall_name)
     ofile << wallet_name << '\n';
     ofile << 0 << ' ' << 0 << '\n';
     ofile.close();
+    history.resize(0);
     std::ofstream outfile,outfile1;
     outfile.open(file_name +"_logs",ios::app);
     outfile1.open("universal_logs",ios::app);
@@ -93,8 +115,167 @@ wallet::wallet(std::string file_name,std::string wall_name)
     outfile1.close();
     return;
 }
-bool wallet::add_entry(std::tuple <int, std::string, double, std::string, std::string, double> tp )
+void wallet::write_wallet()
 {
+    std::ofstream ofile;
+    ofile.open(wallet_file_name);
+    ofile << wallet_name << '\n';
+    ofile << no_of_transactions << ' ' << balance << '\n';
+    for(auto tpl : history)
+    {
+        ofile << tpl[0] << ' ' << tpl[1] << ' ' << tpl[2] << ' ' << tpl[3] << ' ' << tpl[4] << ' ' << tpl[5] << '\n';
+    }
+    ofile.close();
+    std::ofstream outfile,outfile1;
+    outfile.open(wallet_file_name +"_logs",ios::app);
+    outfile1.open("universal_logs",ios::app);
+    time_t curr_time;
+	curr_time = time(NULL);
+	char *tm = ctime(&curr_time);
+    outfile << "Wallet overwritten at time : " << tm << '\n' ;
+    outfile1 << "Wallet file : " << wallet_file_name << " Wallet overwritten at time " << tm << '\n' ;
+    outfile.close();
+    outfile1.close();
+}
+bool wallet::show_wallet()
+{
+    if(!valid_wallet)
+    {
+        return false;
+    }
+    std::cout << "\n\n\n\n\n\n\n\n\n\n\n";
+    std::cout << "Wallet : " << wallet_name << '\n';
+    std::cout << "Number of Transactions : " << no_of_transactions << '\n';
+    std::cout << "Balance : " << balance << '\n';
+    for (auto tpl : history)
+    {
+        std::cout << tpl[0] << ' ' << tpl[1] << ' ' << tpl[2] << ' ' << tpl[3] << ' ' << tpl[4] << ' ' << tpl[5] << '\n';
+    }
+    std::ofstream outfile,outfile1;
+    outfile.open(wallet_file_name +"_logs",ios::app);
+    outfile1.open("universal_logs",ios::app);
+    time_t curr_time;
+	curr_time = time(NULL);
+	char *tm = ctime(&curr_time);
+    outfile << "Wallet shown at time " << tm << '\n' ;
+    outfile1 << "Wallet file : " << wallet_file_name << " shown at time "  << tm << '\n';
+    outfile.close();
+    outfile1.close();
+    return true;
+}
+bool wallet::show_wallet(int a, int b)
+{
+    if(!valid_wallet)
+    {
+        return false;
+    }
+    std::cout << "\n\n\n\n\n\n\n\n\n\n\n";
+    std::cout << "Wallet : " << wallet_name << '\n';
+    std::cout << "Number of Transactions : " << no_of_transactions << '\n';
+    std::cout << "Balance : " << balance << '\n';
+    for (auto tpl : history)
+    {
+        if(stoi(tpl[0]) < a || stoi(tpl[0]) > b)
+        {
+            continue;
+        }
+        std::cout << tpl[0] << ' ' << tpl[1] << ' ' << tpl[2] << ' ' << tpl[3] << ' ' << tpl[4] << ' ' << tpl[5] << '\n';
+    }
+    std::ofstream outfile,outfile1;
+    outfile.open(wallet_file_name +"_logs",ios::app);
+    outfile1.open("universal_logs",ios::app);
+    time_t curr_time;
+	curr_time = time(NULL);
+	char *tm = ctime(&curr_time);
+    outfile << "Wallet shown conditional with S.nos " << a << " to " << b << " at time " << tm << '\n' ;
+    outfile1 << "Wallet file : " << wallet_file_name << " shown conditionally with S.nos " << a << " to " << b << " at time " << tm << '\n' ;
+    outfile.close();
+    outfile1.close();
+    return true;
+}
+bool wallet::search_wallet_exactly(int parameter, std::string str)
+{
+    std::cout << "\n\n\n\n\n\n\n\n\n\n\n";
+    std::ofstream ofile;
+    ofile.open("exact_search_result.txt");
+    std::cout << "Wallet : " << wallet_name << '\n';
+    ofile << "Wallet : " << wallet_name << '\n';
+    std::cout << "Number of Transactions : " << no_of_transactions << '\n';
+    ofile << "Number of Transactions : " << no_of_transactions << '\n';
+    std::cout << "Balance : " << balance << '\n';
+    ofile << "Balance : " << balance << '\n';
+    if(parameter > 6 || parameter < 0)
+    {
+        return false;
+    }
+    for (auto tpl : history)
+    {
+        if(tpl[parameter] != str)
+        {
+            continue;
+        }
+        std::cout << tpl[0] << ' ' << tpl[1] << ' ' << tpl[2] << ' ' << tpl[3] << ' ' << tpl[4] << ' ' << tpl[5] << '\n';
+        ofile << tpl[0] << ' ' << tpl[1] << ' ' << tpl[2] << ' ' << tpl[3] << ' ' << tpl[4] << ' ' << tpl[5] << '\n';
+    }
+    std::ofstream outfile,outfile1;
+    outfile.open(wallet_file_name +"_logs",ios::app);
+    outfile1.open("universal_logs",ios::app);
+    time_t curr_time;
+	curr_time = time(NULL);
+	char *tm = ctime(&curr_time);
+    outfile << "Wallet searched exactly with parameter : " << parameter << " and pattern " << str << " at time " << tm << '\n' ;
+    outfile1 << "Wallet file : " << wallet_file_name << " searched exactly with parameter : " << parameter << " and pattern " << str << " at time " << tm << '\n' ;
+    outfile.close();
+    outfile1.close();
+    return true;
+}
+bool wallet::search_wallet_approx(int parameter, std::string str)//shows chronological transactions container str in the respective parameter
+{
+    std::cout << "\n\n\n\n\n\n\n\n\n\n\n";
+    std::ofstream ofile;
+    ofile.open("exact_search_result.txt");
+    std::cout << "Wallet : " << wallet_name << '\n';
+    ofile << "Wallet : " << wallet_name << '\n';
+    std::cout << "Number of Transactions : " << no_of_transactions << '\n';
+    ofile << "Number of Transactions : " << no_of_transactions << '\n';
+    std::cout << "Balance : " << balance << '\n';
+    ofile << "Balance : " << balance << '\n';
+    if(parameter > 6 || parameter < 0)
+    {
+        return false;
+    }
+    for (auto tpl : history)
+    {
+        if(!is_sub_string(tpl[parameter],str))
+        {
+            continue;
+        }
+        std::cout << tpl[0] << ' ' << tpl[1] << ' ' << tpl[2] << ' ' << tpl[3] << ' ' << tpl[4] << ' ' << tpl[5] << '\n';
+        ofile << tpl[0] << ' ' << tpl[1] << ' ' << tpl[2] << ' ' << tpl[3] << ' ' << tpl[4] << ' ' << tpl[5] << '\n';
+    }
+    std::ofstream outfile,outfile1;
+    outfile.open(wallet_file_name +"_logs",ios::app);
+    outfile1.open("universal_logs",ios::app);
+    time_t curr_time;
+	curr_time = time(NULL);
+	char *tm = ctime(&curr_time);
+    outfile << "Wallet searched approximately with parameter : " << parameter << " and pattern " << str << " at time " << tm << '\n' ;
+    outfile1 << "Wallet file : " << wallet_file_name << " searched approximately with parameter : " << parameter << " and pattern " << str << " at time " << tm << '\n' ;
+    outfile.close();
+    outfile1.close();
+    return true;
+}
+bool sort_wallet(int parameter)
+{
+    //not_implemented
+}
+bool wallet::add_entry(std::vector<std::string> tp)
+{
+    if(!valid_wallet)
+    {
+        return false;
+    }
+    tp[5] = to_string(stod(tp[2])+balance); 
     history.push_back(tp);
     std::ofstream outfile,outfile1;
     outfile.open(wallet_file_name +"_logs",ios::app);
@@ -102,33 +283,33 @@ bool wallet::add_entry(std::tuple <int, std::string, double, std::string, std::s
     time_t curr_time;
 	curr_time = time(NULL);
 	char *tm = ctime(&curr_time);
-    outfile << "Wallet entry : " << get<0>(tp) << ' ' << get<1>(tp) << ' ' << get<2>(tp) << ' '<< get<3>(tp) << ' ' << get<4>(tp) << ' ' << get<5>(tp) << ' '  << " added at time " << tm << '\n' ;
-    outfile << "Wallet file : " << wallet_file_name << " Wallet entry : " << get<0>(tp) << ' ' << get<1>(tp) << ' ' << get<2>(tp) << ' '<< get<3>(tp) << ' ' << get<4>(tp) << ' ' << get<5>(tp) << ' '  << " added at time " << tm << '\n' ;
+    outfile << "Wallet entry : " << tp[0] << ' ' << tp[1] << ' ' << tp[2] << ' '<< tp[3] << ' ' << tp[4] << ' ' << tp[5] << ' '  << " added at time " << tm << '\n' ;
+    outfile1 << "Wallet file : " << wallet_file_name << " Wallet entry : " << tp[0] << ' ' << tp[1] << ' ' << tp[2] << ' '<< tp[3] << ' ' << tp[4] << ' ' << tp[5] << ' '  << " added at time " << tm << '\n' ;
     outfile.close();
     outfile1.close();
+    write_wallet();
     return true;
 }
-bool wallet::show_wallet()
+bool wallet::delete_entry(int s_no)
 {
-    if(!valid_wallet)
+    int i = 0;
+    history.erase(history.begin()+s_no-1);
+    for(int i=s_no-1;i<history.size();i++)
     {
-        return;
+        history[i][0] = to_string(stoi(history[i][0])-1);
     }
-    std::cout << "\n\n\n\n\n\n\n\n\n\n\n";
-    std::cout << "Wallet : " << wallet_name << '\n';
-    std::cout << "Number of Transactions : " << no_of_transactions << '\n';
-    std::cout << "Balance : " << balance << '\n';
-
     std::ofstream outfile,outfile1;
     outfile.open(wallet_file_name +"_logs",ios::app);
     outfile1.open("universal_logs",ios::app);
     time_t curr_time;
 	curr_time = time(NULL);
 	char *tm = ctime(&curr_time);
-    outfile << "Wallet accessed at time " << tm << '\n' ;
-    outfile1 << "Wallet file : " << file_name << " accessed at time "  << tm << '\n';
+    outfile << "Wallet entry : " << tp[0] << ' ' << tp[1] << ' ' << tp[2] << ' '<< tp[3] << ' ' << tp[4] << ' ' << tp[5] << ' '  << " added at time " << tm << '\n' ;
+    outfile1 << "Wallet file : " << wallet_file_name << " Wallet entry : " << tp[0] << ' ' << tp[1] << ' ' << tp[2] << ' '<< tp[3] << ' ' << tp[4] << ' ' << tp[5] << ' '  << " added at time " << tm << '\n' ;
     outfile.close();
     outfile1.close();
+    write_wallet();
+    return true;
 }
 wallet::~wallet()
 {
--- wallet.h
+++ wallet.h
@@ -11,18 +11,19 @@ private:
     bool valid_wallet;
     double balance;
     int no_of_transactions;
-    std::vector <std::tuple <int, std::string, double, std::string, std::string, double> > history;
+    std::vector<std::vector<std::string> > history;
 public:
     wallet(std::string);//initializes pre_existing wallet
     wallet(std::string,std::string);//initializes new wallet
+    void write_wallet();
     bool show_wallet();//shows all the transactions
     bool show_wallet(int a, int b);//shows the entries from serial number a to serial number b
     bool search_wallet_exactly(int parameter,std::string str);//shows chronological transactions matching str exactly in the respective parameter
     bool search_wallet_approx(int parameter, std::string str);//shows chronological transactions container str in the respective parameter
     bool sort_wallet(int parameter);//sorts the wallet by the given parameter
     bool generate_wallet_file(std::string);//puts the wallet in an organised text file
-    bool add_entry(std::tuple <int, std::string, double, std::string, std::string, double> );// adds an entry into the wallet
-    bool delete_entry(int s_no, std::string file_name);//deletes the s_no'th entry from the wallet
+    bool add_entry(std::vector<std::string>);// adds an entry into the wallet
+    bool delete_entry(int s_no);//deletes the s_no'th entry from the wallet
     bool show_local_logs();//shows current wallet editing logs
     bool show_universal_logs();//shows universal wallet editing logs
     //void statistics();//To be implemented in future
